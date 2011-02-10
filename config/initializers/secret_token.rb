fcac5514dd83aca34337a9acfd3d74651ddccfb2
daniel lewis <daniel@cloudspace.com>
2011-02-10 17:24:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeatureGirl::Application.config.secret_token = 'a414158f825d5d08ff2ddbc089a1acafc58ca08374a23045caf16e7846111f47565494c71411b8d5d0192a9818ada0d3e6035c5e4bf1cc878803c1a7265ebe62'
