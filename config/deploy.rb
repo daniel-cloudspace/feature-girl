b52ebd5afc4d33dcb63d7a96f5d1471f7c72c023
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-02-17 13:06:33 -0500

    Working on cap deploy file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,35 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "feature-girl"
+set :repository,  "git@github.com:daniel-cloudspace/feature-girl.git"
+set :scm, :git
+set :branch, "capistrano" #currently working off of the capistrano branch because I don't want to mess with master
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_to, "/vol/www/#{application}"
+set :deploy_via, :remote_cache
+set :rails_env, "production"
+set :ssh_options, { :user => "root", :password => "kijiop" }
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "local.feature-girl.com"                          # Your HTTP server, Apache/etc
+role :app, "local.feature-girl.com"                          # This may be the same as your `Web` server
+role :db,  "local.feature-girl.com", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :bundler do
+  task :bundle_new_release do
+    #bundler.create_symlink
+    #bundler.install
+    run "cd #{release_path} && bundle isntall"
+  end
+end
+
+after "deploy", "bundler.bundle_new_release"
