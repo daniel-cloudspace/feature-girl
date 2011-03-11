d86d9474788418bc5421b7efcf1907fc2d8b7f10
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-03-11 09:57:29 -0500

    trying to figur eout deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,8 +13,7 @@ set :branch, "capistrano" #currently working off of the capistrano branch becaus
 set :deploy_to, "/vol/www/#{application}"
 set :deploy_via, :remote_cache
 set :rails_env, "production"
-#set :ssh_options, {:user => "vagrant" }
-ssh_options[:keys] = `vagrant ssh_config | grep IdentityFile`.split.last
+set :ssh_options, {:forward_agent => true, :user => "vagrant" }
 
 role :web, "local.feature-girl.com"                          # Your HTTP server, Apache/etc
 role :app, "local.feature-girl.com"                          # This may be the same as your `Web` server
